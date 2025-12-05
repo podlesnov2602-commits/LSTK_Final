32c067f1ac5155393fb6ebb028b1b85447fc40c3
podlesnov2602-commits <podlesnov2602@gmail.com>
2025-12-05 18:03:38 +0500

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -254,15 +254,15 @@ const Home = () => {
               </button>
             </div>
 
-            <div className="contact-map">
+           <div className="contact-map">
               <iframe
                 title="Location Map"
-                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2906.25!2d${siteConfig.location.lng}!3d${siteConfig.location.lat}!2m3!1f0!2f0!3f0`}
+                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2906.2524871691937!2d${siteConfig.location.lng}!3d${siteConfig.location.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDPCsDEzJzE5LjIiTiA3NsKwNTEnMDQuMyJF!5e0!3m2!1sen!2skz!4v1234567890123!5m2!1sen!2skz`}
                 width="100%"
                 height="400"
                 style={{ border: 0, borderRadius: '16px' }}
+                allowFullScreen=""
                 loading="lazy"
-                allowFullScreen
               />
             </div>
 
